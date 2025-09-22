143b740afa049a8c695b19919771774f51aa3c79
unknown <efrenpastores@gmail.com>
2025-09-22 20:44:36 +0800

    feat: fix

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -232,6 +232,12 @@ export const ChevronDownIcon = ({ className }: { className?: string }) => (
     </svg>
 );
 
+export const ChevronUpIcon = ({ className }: { className?: string }) => (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <polyline points="6 15 12 9 18 15"></polyline>
+    </svg>
+);
+
 export const XCircleIcon = ({ className }: { className?: string }) => (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <circle cx="12" cy="12" r="10"></circle>
